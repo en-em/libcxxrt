28afe3d5da798e7357c5f63b8e7b60c949690186
en-em <i.hamsa@gmail.com>
2017-04-22 18:22:07 +0300

    Fixed warning about undeclared asprintf

--- src/libelftc_dem_gnu3.c
+++ src/libelftc_dem_gnu3.c
@@ -23,15 +23,18 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <sys/types.h>
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
 #include <limits.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 
 /**
  * @file cpp_demangle.c
